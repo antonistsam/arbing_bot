40039e3b80815c38cbc07f8a54295853bc78aaa4
DESKTOP-UG5V55N\user <tony.tsam40@gmail.com>
2024-08-02 22:49:59 +0300

    mapper init

---
+++ mapper.py
@@ -0,0 +1,5 @@
+mapping_dict = {
+    ('AH1', '+0.5', None): 'Asian Handicap (Current Score 0 - 0)', 
+    ('AH1', '-0.5', None): 'Asian Handicap (Current Score 0 - 0)',
+    # Add more mappings as required
+}
